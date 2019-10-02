f0c3460e41e30d0ca6e69eb8c978cf7467578638
SeparateRecords <SeparateRecords@users.noreply.github.com>
2019-10-02 23:26:52 +1000

    Bump version to 1.0.0a1 (for testing, no release)

--- nightcore/__init__.py
+++ nightcore/__init__.py
@@ -9,7 +9,7 @@ from typing import Optional
 
 from pydub import AudioSegment
 
-__version__ = "0.5.6"
+__version__ = "1.0.0a1"
 
 __all__ = ("Nightcore", "Semitones", "Tones", "Octaves", "Percent")
 
