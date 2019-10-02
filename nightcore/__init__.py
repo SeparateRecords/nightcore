54b25c99818727a1e79fbc104f52303513f16890
SeparateRecords <SeparateRecords@users.noreply.github.com>
2019-10-02 13:00:25 +1000

    Add operators on RelativeChange subclasses Only __add__ and __sub__ are implemented on BaseInterval, as it would not make sense to implement multiplication, division, etc.

--- nightcore/__init__.py
+++ nightcore/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import operator
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import wraps
 from os import PathLike
-from typing import Optional
+from typing import Optional, Union
 
 from pydub import AudioSegment
 
@@ -21,17 +22,41 @@ class _NameTypeMap(dict):
 step_types = _NameTypeMap()
 
 
-@dataclass
+@dataclass(frozen=True, eq=False)
 class RelativeChange(ABC):
     """Convert numerical values to an amount of change"""
 
-    amount: float
+    amount: Union[int, float]
 
     @abstractmethod
     def as_percent(self) -> float:
         """Returns a percentage change, as a float (1.0 == 100%).
         Note that 1.0 represents no change (5 * 1.0 == 5)"""
         raise NotImplementedError
+
+    def __neg__(self):
+        return self.__class__(-self.amount) if self.amount > 0 else self
+
+    def __pos__(self):
+        return self.__class__(+self.amount) if self.amount < 0 else self
+
+    def __eq__(self, other):
+        return self.as_percent() == other
+
+    def __lt__(self, other):
+        return self.as_percent() < other
+
+    def __le__(self, other):
+        return self.as_percent() <= other
+
+    def __gt__(self, other):
+        return self.as_percent() > other
+
+    def __ge__(self, other):
+        return self.as_percent() >= other
+
+    def __int__(self):
+        return int(self.as_percent())
 
     def __float__(self):
         return self.as_percent()
@@ -47,6 +72,26 @@ class BaseInterval(RelativeChange):
 
     def as_percent(self) -> float:
         return 2 ** (self.amount / self.n_per_octave)
+
+    def _math_operation(self, op, other):
+        cls = self.__class__
+
+        try:
+            n_of_self_in_other = self.n_per_octave / other.n_per_octave
+            other_amt = other.amount * n_of_self_in_other
+        except AttributeError:
+            other_amt = other
+
+        try:
+            return cls(op(self.amount, other_amt))
+        except TypeError:
+            return NotImplemented
+
+    def __add__(self, other):
+        return self._math_operation(operator.add, other)
+
+    def __sub__(self, other):
+        return self._math_operation(operator.sub, other)
 
 
 @step_types.add
@@ -76,6 +121,42 @@ class Percent(RelativeChange):
 
     def as_percent(self) -> float:
         return self.amount / 100
+
+    def __add__(self, other):
+        return self.__class__(self.amount + other)
+
+    def __radd__(self, other):
+        return other + self.amount
+
+    def __sub__(self, other):
+        return self.__class__(self.amount - other)
+
+    def __rsub__(self, other):
+        return other - self.amount
+
+    def __mul__(self, other):
+        return self.__class__(self.amount * other)
+
+    def __rmul__(self, other):
+        return other * self.as_percent()
+
+    def __truediv__(self, other):
+        return self.__class__(self.amount / other)
+
+    def __rtruediv__(self, other):
+        return other / self.as_percent()
+
+    def __floordiv__(self, other):
+        return self.__class__(self.amount // other)
+
+    def __rfloordiv__(self, other):
+        return other // self.as_percent()
+
+    def __mod__(self, other):
+        return self.__class__(self.amount % other)
+
+    def __rmod__(self, other):
+        return other % self.as_percent()
 
 
 class Nightcore:
