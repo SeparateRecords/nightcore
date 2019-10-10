21c05dd62121926a1c8326151c6529f78ba16d1a
SeparateRecords <SeparateRecords@users.noreply.github.com>
2019-10-10 12:22:28 +1100

    Use click.get_text_stream instead of sys.stdout

--- nightcore/cli.py
+++ nightcore/cli.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from sys import stdout
-
 import click
 
 import nightcore as nc
 
 change_classes = [nc.Octaves, nc.Tones, nc.Semitones, nc.Percent]
 amount_types = {cls.__name__.lower(): cls for cls in change_classes}
+
+stdout = click.get_text_stream("stdout")
 
 
 @click.command(context_settings={"ignore_unknown_options": True})
